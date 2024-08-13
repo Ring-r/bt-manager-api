0459cdf0bab8d1426d3954e5a14b93584a246d51
Rostyslav Pudlo <rostyslav.pudlo@gmail.com>
2024-08-13 21:30:13 +0300

    use decorator instead of manually adding tasks to factory

--- tasks.py
+++ tasks.py
@@ -8,12 +8,20 @@ from typing import Callable
 logger = logging.getLogger(__name__)
 
 
+factory: dict[str, Callable[..., asyncio.Task]] = {}
+
+
+def add_to_factory(name: str | None = None):
+    def decorator(func):
+        factory[name if name is not None else func.__name__] = func
+        return func
+
+    return decorator
+
+
+@add_to_factory()
 def sleep_task(delay: float) -> asyncio.Task:
     return asyncio.create_task(asyncio.sleep(delay), name=sleep_task.__name__)
 
-
-factory: dict[str, Callable] = {
-    sleep_task.__name__: sleep_task,
-}
 
 logger.warning(json.dumps({task_name: "" for task_name in factory}, indent=2))
